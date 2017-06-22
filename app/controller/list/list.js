e89db831f0a73476743de2d4898078a61717c2ff
wuhui.zhu <wuhui.zhu@platenogroup.com>
2017-06-22 18:44:04 +0800

    增加测试组件；增加验证组件；修改登录验证中间件；

--- app/controller/list/list.js
+++ app/controller/list/list.js
@@ -1,8 +1,14 @@
-exports.index = function* () {
-
-    // this.session.user = null;
-    let user = this.session.user || {};
-    yield this.render('list/list.js', {
-        userName: user.userName || '游客'
-    })
+module.exports = app => {
+    return class ListController extends app.Controller {
+        async index(ctx) {
+            // let Good = ctx.model.Good;
+            // this.session.user = null;
+            // let result = await Good.find({});
+            console.log('验证函数', ctx.validator());
+            // let user = this.session.user || {};
+            // await this.render('list/list.js', {
+            //     userName: user.userName || '游客'
+            // })
+        }
+    }
 }
--- app/middleware/auth.js
+++ app/middleware/auth.js
@@ -1,6 +1,7 @@
 module.exports = () => {
-    return async function (ctx, next) {
+    return async function(ctx, next) {
         if (ctx.session != undefined && ctx.session.user) {
+            console.log('ddd');
             await next();
         } else {
             ctx.redirect('/');
